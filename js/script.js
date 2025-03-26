12d05887134803acde53585b4fdb233ce3e7238f
mannuccimattia <mannuccimattia@gmail.com>
2025-03-26 16:18:21 +0100

    [UPD] Done Milestone 2

--- js/script.js
+++ js/script.js
@@ -3,11 +3,16 @@
 /* ******************************************** */
 // DEFINIZIONE DELLE VARIABILI E/O RECUPERO ELEMENTI DEL DOM
 // recupero i campi della form
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
 const km = document.getElementById("km");
 const age = document.getElementById("age");
 const button = document.querySelector("button");
 
-
+// recupero i segnaposto
+const fullName = document.getElementById("full-name");
+const ticketKm = document.getElementById("ticket-km");
+let ticketPrice = document.getElementById("ticket-price");
 
 // CORPO DEL PROGRAMMA
 // al click del bottone, calcola il prezzo del biglietto
@@ -19,15 +24,19 @@ button.addEventListener("click", function (event) {
   let seniorPrice = price * 0.6;
   let userPrice = 0;
 
-  // controllo l'età e stampo il rispettivo prezzo
+  // controllo l'età e stampo in console il rispettivo prezzo
   userPrice = age.value<18 ? juniorPrice
   : age.value>65 ? seniorPrice
   : price;
   console.log("Il prezzo del tuo biglietto è di",userPrice.toFixed(2).toString(), "€");
 
+  // assegno i valori inseriti e calcolati nella card
+  fullName.innerHTML = firstName.value.trim() + " " + lastName.value.trim();
+  ticketKm.innerHTML = km.value + " km";
+  ticketPrice.innerHTML = userPrice.toFixed(2).toString() + " €";
   // azzero i valori inseriti dall'utente nei campi input
+  firstName.value = "";
+  lastName.value = "";
   km.value = "";
   age.value= "";
-
-  //
 });
